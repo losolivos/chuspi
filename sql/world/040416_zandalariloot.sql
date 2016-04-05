290390ded60008559786a13d493b15d4a89508e2
chamorrorenzo <trixi2@hotmail.com>
2016-04-04 23:29:59 -0500

    SQL/Custom: zandalari drop y npc promo correct scriptname

---
+++ sql/world/040416_zandalariloot.sql
@@ -0,0 +1,5 @@
+-- zandalari drop
+UPDATE creature_template SET lootid = "69841" WHERE entry = 69769;
+
+-- me huevie de script en npc promo
+UPDATE creature_template SET ScriptName = "npc_new_players_giftt" WHERE entry = 911119;
