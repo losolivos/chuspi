deab349bce66611ccbbdd4e4fa4c2c7340e81b56
chamorrorenzo <trixi2@hotmail.com>
2016-04-07 00:14:02 -0500

    DB/SQL: Fix required rep for Serpent Cloud faction

---
+++ sql/world/060416_Elder_Anli.sql
@@ -0,0 +1,13 @@
+-- ELder Anli 58564
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
+(58564,31194);
+INSERT INTO `creature_questender` (`id`, `quest`) VALUES
+(58564,31194);
+
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31701;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31715;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31703;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31705;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31702;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31711;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31194;
---
+++ sql/world/060416_skythron_daylifix.sql
@@ -0,0 +1,7 @@
+-- Skythron daylis fix
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31707;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=30158;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31712;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=30155;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31698;
+UPDATE quest_template SET RequiredMinRepValue=9000 WHERE Id=31706;
